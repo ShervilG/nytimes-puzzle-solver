cb28cff53ffbc897af2b62da3cd0ffaf0ba7d853
ShervilG <wintergpt45@gmail.com>
2024-09-07 18:01:00 +0530

    Added mini-crossword solution

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
-wordle_url = "https://www.nytimes.com/svc/wordle/v2/";
-connections_url = "https://www.nytimes.com/svc/connections/v2/";
+const WORDLE_URL = "https://www.nytimes.com/svc/wordle/v2";
+const CONNECTIONS_URL = "https://www.nytimes.com/svc/connections/v2";
+const MINI_CROSSWORD_URL = "https://www.nytimes.com/svc/crosswords/v6/puzzle/mini";
 
 const today = new Date();
 const year = today.getFullYear();
@@ -7,41 +8,74 @@ const month = String(today.getMonth() + 1).padStart(2, '0');
 const day = String(today.getDate()).padStart(2, '0');
 const formattedDate = `${year}-${month}-${day}`;
 
-function findWordleSolution() {
-    fetch(`${wordle_url}${formattedDate}.json`, {
-        method: 'GET'
-    }).then(res => {
-        res.json().then(d => {
-            const p = document.createElement("p");
-            p.innerHTML = d.solution;
+function createUrl(baseUrl, date) {
+    if (date == null) {
+        return `${baseUrl}.json`;    
+    }
 
-            const el = document.getElementsByClassName("sol")[0];
-            el.innerHTML = '';
-            el.appendChild(p);
-        });
+    return `${baseUrl}/${date}.json`;
+}
+
+function makeJsonFetchRequest(url, callback) {
+    fetch(url, {method: 'GET'}).then(data => data.json().then(d => callback(d)));
+}
+
+function findWordleSolution() {
+    makeJsonFetchRequest(createUrl(WORDLE_URL, formattedDate), (d) => {
+        const p = document.createElement("p");
+        p.innerHTML = d.solution;
+
+        const el = document.getElementsByClassName("sol")[0];
+        el.innerHTML = '';
+        el.appendChild(p);
     });
 }
 
 function findConnectionsSolution() {
-    fetch(`${connections_url}${formattedDate}.json`, {
-        method: 'GET'
-    }).then(res => {
-        res.json().then(d => {
-            const ps = [];
-            d.categories.forEach(category => {
-                const p = document.createElement("p");
-                p.innerHTML = `${category.cards[0].content}, ${category.cards[1].content}, ${category.cards[2].content}, ${category.cards[3].content}`;
+    makeJsonFetchRequest(createUrl(CONNECTIONS_URL, formattedDate), (d) => {
+        const ps = [];
+        d.categories.forEach(category => {
+            const p = document.createElement("p");
+            p.innerHTML = `${category.cards[0].content}, ${category.cards[1].content}, ${category.cards[2].content}, ${category.cards[3].content}`;
 
-                ps.push(p);
-            });
-
-
-            const el = document.getElementsByClassName("sol")[0];
-            el.innerHTML = '';
-            ps.forEach(p => {
-                el.appendChild(p);
-            })
+            ps.push(p);
         });
+
+
+        const el = document.getElementsByClassName("sol")[0];
+        el.innerHTML = '';
+        ps.forEach(p => {
+            el.appendChild(p);
+        })
+    });
+}
+
+function findMiniCrossWordSolution() {
+    makeJsonFetchRequest(createUrl(MINI_CROSSWORD_URL, null), (d) => {
+        const body = d.body[0];
+        const { height, width } = body.dimensions;
+        const cells = body.cells;
+        
+        const ps = [];
+        for (let i = 0;i < height;i++) {
+            const p = document.createElement("p");
+            let content = '';
+
+            for (let j = 0;j < width;j++) {
+                const index = i * width + j;
+
+                content += (cells[index].answer ?? '_');
+            }
+
+            p.innerHTML = content;
+            ps.push(p);
+        }
+        
+        const el = document.getElementsByClassName("sol")[0];
+        el.innerHTML = '';
+        ps.forEach(p => {
+            el.appendChild(p);
+        })
     });
 }
 
@@ -53,6 +87,9 @@ function getSolutionForGame() {
             break;
         case "connections":
             findConnectionsSolution();
+            break;
+        case "mini":
+            findMiniCrossWordSolution();
             break;
         default:
             break;
